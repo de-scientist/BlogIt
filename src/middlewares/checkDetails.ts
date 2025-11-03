ba03daf54231cc8c9253b2d25d55e42927d480a9
de-scientist <gitaumark502@gmail.com>
2025-11-03 12:03:11 +0300

    check if first name is entered

--- src/middlewares/checkDetails.ts
+++ src/middlewares/checkDetails.ts
@@ -3,5 +3,11 @@ import { type Request, type Response, type NextFunction } from "express";
 export function checkDetails(req: Request, res: Response, next: NextFunction) {
     const { firstName, lastName, userName, emailAddress, password } = req.body;
 
-    if
+    //check if first name is entered
+    if (!firstName) {
+        res.status(400).json({ message:"First name is required" });
+        return;
+    }
+
+    //check 
 }
