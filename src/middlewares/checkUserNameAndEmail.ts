3e208735f964c158084d00034ab750efe0b83546
de-scientist <gitaumark502@gmail.com>
2025-11-03 12:24:46 +0300

    check the email address if its already in use

--- src/middlewares/checkUserNameAndEmail.ts
+++ src/middlewares/checkUserNameAndEmail.ts
@@ -3,3 +3,19 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
+export async function checkUserAndEmail(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { emailAddress, userName } = req.body;
+
+        const userWithEmail = await client.user.findUnique({
+            where: { emailAddress },
+        });
+        if (userWithEmail) {
+            res.status(400).json({ message: "The email you have provided is already associated with an account."});
+            return;
+        }
+        
+    } catch (e) {
+        
+    }
+}
