6a61bdf94b7ab5c5c8d0a133e9c9680a6e0c1d27
de-scientist <gitaumark502@gmail.com>
2025-11-03 12:31:55 +0300

    create a file to check password strength

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { type Express, type Request, type Response } from "express";
 import dotenv from "dotenv";
 import { checkDetails } from "./middlewares/checkDetails.ts";
+import { checkUserAndEmail } from "./middlewares/checkUserNameAndEmail.ts";
 import { register } from "./controllers/auth.ts";
-
 
 
 const app = express();
@@ -14,7 +14,7 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 //add the user route handlers
-app.post("/auth/register",checkDetails, register)
+app.post("/auth/register",checkDetails, checkUserAndEmail, register)
 
 
 
