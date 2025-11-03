bbdeb8a3c7390184e60259192ddc4d49fd0567ba
de-scientist <gitaumark502@gmail.com>
2025-11-03 11:25:19 +0300

    import jwt and bcrypt

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response } from "express";
 import { PrismaClient } from "@prisma/client/extension";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const client = PrismaClient();
