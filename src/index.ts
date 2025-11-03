86246b909d1531eaa266dc21115c5355c3c2e6d0
de-scientist <gitaumark502@gmail.com>
2025-11-03 10:25:22 +0300

    format code using prettier --write .

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express, { type Express, type Request, type Response } from "express";
 
 const app: Express = express();
 
-app.get("/", (_req: Request, res: Response) => { res.send("Welcome to Express + TS"); });
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Welcome to Express + TS");
+});
 
-const PORT = 3000; app.listen(PORT, () => { console.log(`App running on port ${PORT}`); }); 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
