f3df82853247116b72525f5e343ae7f4a6ca6161
de-scientist <gitaumark502@gmail.com>
2025-11-03 11:37:41 +0300

    export the register function

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,32 @@
 import { type Request, type Response } from "express";
 import { PrismaClient } from "@prisma/client/extension";
-import jwt from "jsonwebtoken";
+//import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 const client = PrismaClient();
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { firstName, lastName, userName, emailAddress, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 11);
+
+        await client.user.create({
+            data: {
+                firstName,
+                lastName,
+                userName,
+                emailAddress,
+                password: hashedPassword
+            },
+        });
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (e: any) {
+        if (e.code === "P2002")
+        {
+return res.status(409).json({ message: "Username or email already exists"
+});
+        }
+         res.status(500).json({ message: "Something went wrong! Please try again."});   
+        
+    }
+};
