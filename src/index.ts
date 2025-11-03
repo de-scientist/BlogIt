e9012f6941cb986ee7e14b73a590302b0241240c
de-scientist <gitaumark502@gmail.com>
2025-11-03 11:40:17 +0300

    import the register controller function from the auth.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { type Express, type Request, type Response } from "express";
+import { register } from "./controllers/auth.ts";
+
 
 const app: Express = express();
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Welcome to Express + TS");
 });
+
+//add the user route handlers
+app.post("/auth/register", register)
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
