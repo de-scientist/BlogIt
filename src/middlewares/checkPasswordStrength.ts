9ac1b55ff7a143e2282a67efd8c7a423e2ecd728
de-scientist <gitaumark502@gmail.com>
2025-11-03 12:39:41 +0300

    validate presence of a password

--- src/middlewares/checkPasswordStrength.ts
+++ src/middlewares/checkPasswordStrength.ts
@@ -1,3 +1,12 @@
 import { type Request, type Response, type NextFunction } from "express";
 import zxcvbn from "zxcvbn";
 
+export function checkPasswordStrength(req: Request, res: Response, next: NextFunction) {
+    const { password } = req.body;
+
+    //validate presence of a password
+    if (!password || typeof password !== "string" ) {
+        return res.status(400).json({ message: "Password is required and must be a string."});
+    }
+    
+}
