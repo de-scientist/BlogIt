c3108fdc917a8b8532466497b32df6b343cd6f13
de-scientist <gitaumark502@gmail.com>
2025-11-04 09:37:25 +0300

    export the verifyToken middleware function

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,19 @@
+import { type Request, type Response, type NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+    const { authToken } = req.cookies;
+
+    if (!authToken) {
+        res.status(401).json({ message: "Unauthorized! Please login."});
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(authToken, process.env.JWT_SECRET_KEY!);
+        req.user = decoded as User;
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong! Please try again."});
+    }
+    next();
+}
