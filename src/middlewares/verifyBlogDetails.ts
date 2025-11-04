edc2130a8b1a1718c4e80b63fac83e21bba6dabe
de-scientist <gitaumark502@gmail.com>
2025-11-04 10:05:36 +0300

    check if title is provided

---
+++ src/middlewares/verifyBlogDetails.ts
@@ -0,0 +1,16 @@
+import { type Request, type Response, type NextFunction } from "express";
+
+export const validateBlogDetails = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { title, description } = req.body;
+
+        //check if title is provided
+        if (!title) {
+            res.status(400).json({ message: "Title is required" });
+            return;
+        }
+        
+    } catch (error) {
+        
+    }
+}
