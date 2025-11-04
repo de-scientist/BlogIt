63f290f13fafbaa1fbc2a7cd15f4d08b81534bfd
de-scientist <gitaumark502@gmail.com>
2025-11-04 09:55:12 +0300

    declare type user

--- src/user.d.ts
+++ src/user.d.ts
@@ -1,3 +1,19 @@
 import * as express from "express";
 
 //create interface user
+interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    emailAddress: string;
+}
+
+//declare type user
+declare global {
+    namespace Express {
+        interface Request {
+            user: User;
+        }
+    }
+}
