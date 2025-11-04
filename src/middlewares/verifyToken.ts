c98c5f0f074fbb23250126d3528fbe60a9132944
de-scientist <gitaumark502@gmail.com>
2025-11-04 09:59:39 +0300

    add interface User in the verify token function

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,6 +1,15 @@
 import { type Request, type Response, type NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+//create interface user
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  emailAddress: string;
+}
+
 export function verifyToken(req: Request, res: Response, next: NextFunction) {
     const { authToken } = req.cookies;
 
