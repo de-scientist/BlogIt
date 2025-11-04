4518bdae28c5f457e9f9e28382992571990a2c19
de-scientist <gitaumark502@gmail.com>
2025-11-04 10:20:53 +0300

    export the create a blog controller function

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -2,3 +2,22 @@ import { type Request, type Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
+
+//create a blog
+export const createBlog = async (req: Request, res: Response) => {
+    try {
+        const { title, description } = req.body;
+
+        await client.blog.create({
+            data: {
+                title,
+                description,
+                userId: req.user.id,
+            },
+        });
+        res.status(201).json({ message: "Blog created successfully"});
+        return;
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong! Kindly try again."});
+    }
+};
