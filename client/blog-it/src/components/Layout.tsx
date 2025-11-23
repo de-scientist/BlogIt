8db0eeefebcb52f016a5a9fdde6582d041a47c66
de-scientist <gitaumark502@gmail.com>
2025-11-23 23:18:40 +0300

    export the layout function component

---
+++ client/blog-it/src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+return (
+<div className="min-h-screen bg-slate-50 dark:bg-slate-900">
+<Navbar />
+<main className="container mx-auto px-4 py-8">{children}</main>
+</div>
+);
+}
