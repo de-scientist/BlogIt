dc218f7f1eea7e6476f3bdf4b5ab6d2e53b6f613
de-scientist <gitaumark502@gmail.com>
2025-11-23 23:31:26 +0300

    export the new blog function component

---
+++ client/blog-it/src/pages/blogs/NewBlog.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import api from '../../lib/axios';
+import { useRouter } from 'next/router';
+
+
+export default function NewBlog() {
+const [title, setTitle] = useState('');
+const [content, setContent] = useState('');
+const router = useRouter();
+
+
+const submit = async (e: React.FormEvent) => {
+e.preventDefault();
+try {
+await api.post('/blogs', { title, content });
+router.push('/blogs/list');
+} catch (err) {
+console.error(err);
+}
+};
+
+
+return (
+<form onSubmit={submit} className="max-w-2xl">
+<input value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" />
+<textarea value={content} onChange={e => setContent(e.target.value)} placeholder="Content" />
+<button type="submit">Create</button>
+</form>
+);
+}
