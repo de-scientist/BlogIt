6d9aabc72eab67e218ebf1da39d146c9e3947517
de-scientist <gitaumark502@gmail.com>
2025-11-24 16:38:20 +0300

    import { Link } from "react-router-dom";

--- client/src/pages/blogs/List.tsx
+++ client/src/pages/blogs/List.tsx
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "@/lib/axios";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { Blog } from "@/types";
 
