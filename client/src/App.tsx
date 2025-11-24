e42d580e913de6789217f474e2d52dfff5bbbd80
de-scientist <gitaumark502@gmail.com>
2025-11-24 11:37:43 +0300

    import { queryClient } from "./lib/queryClient"; in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import { queryClient } from "./lib/react-query";
+import { queryClient } from "./lib/queryClient";
 import { api } from "./lib/axios";
 import { useAuth } from "./store/authStore";
 import Layout from "./components/Layout";
