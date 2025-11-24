c7f2181ac4211a39de0b8e74476147caa8eef9ac
de-scientist <gitaumark502@gmail.com>
2025-11-24 16:17:42 +0300

    update the blog list

--- client/src/pages/blogs/List.tsx
+++ client/src/pages/blogs/List.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/lib/axios";
 import { Link } from "react-router-dom";
+import BlogCard from "@/components/BlogCard";
 
 type Blog = {
   id: string;
@@ -8,10 +9,7 @@ type Blog = {
   synopsis: string;
   featuredImageUrl: string;
   createdAt: string;
-  user: {
-    firstName: string;
-    lastName: string;
-  };
+  user: { firstName: string; lastName: string };
 };
 
 async function fetchBlogs() {
@@ -34,31 +32,31 @@ export default function BlogList() {
           </button>
         </Link>
       </div>
-
       <div className="grid gap-4">
         {data?.map((b) => (
-          <div key={b.id} className="p-4 border rounded shadow-sm">
-            {b.featuredImageUrl && (
-              <img src={b.featuredImageUrl} alt={b.title} className="w-full h-40 object-cover mb-2 rounded" />
-            )}
-            <h3 className="font-bold">{b.title}</h3>
-            <p className="text-sm text-gray-600">{b.synopsis}</p>
-            <p className="text-xs text-gray-500 mt-1">
-              By {b.user.firstName} {b.user.lastName} on {new Date(b.createdAt).toLocaleDateString()}
-            </p>
-            <div className="mt-2 flex gap-2">
-              <Link to={`/blogs/edit/${b.id}`}>
-                <button className="px-2 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600">
-                  Edit
-                </button>
-              </Link>
-              <Link to={`/blogs/trash`}>
-                <button className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600">
-                  Trash
-                </button>
-              </Link>
-            </div>
-          </div>
+          <BlogCard
+            key={b.id}
+            id={b.id}
+            title={b.title}
+            synopsis={b.synopsis}
+            featuredImageUrl={b.featuredImageUrl}
+            authorName={`${b.user.firstName} ${b.user.lastName}`}
+            createdAt={b.createdAt}
+            actions={
+              <>
+                <Link to={`/blogs/edit/${b.id}`}>
+                  <button className="px-2 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600">
+                    Edit
+                  </button>
+                </Link>
+                <Link to="/blogs/trash">
+                  <button className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600">
+                    Trash
+                  </button>
+                </Link>
+              </>
+            }
+          />
         ))}
       </div>
     </div>
