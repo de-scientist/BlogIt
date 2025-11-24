335e01c53453dd2e2860f2e7c303b043e0cb542c
de-scientist <gitaumark502@gmail.com>
2025-11-24 09:29:36 +0300

    import api from "./lib/axios";

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../lib/reactQuery";
 import Layout from "../components/Layout";
 import { useEffect } from "react";
-import api from "../lib/axios";
-import { useAuthStore } from "../hooks/useAuthStore";
+import api from "./lib/axios";
+import { useAuthStore } from "./store/useAuthStore";
 
 
 export default function App({ Component, pageProps }: AppProps) {
