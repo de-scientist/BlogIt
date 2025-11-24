d06b38aab7bba1e5ad95b533b22ced42a0063e33
de-scientist <gitaumark502@gmail.com>
2025-11-24 11:35:51 +0300

    import { useAuth } from "./store/authStore"; in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,32 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "../lib/react-query";
-import Layout from "./components/Layout";
+import "./styles/globals.css";
 import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClient } from "./lib/react-query";
 import api from "./lib/axios";
-import { useAuthStore } from "./store/authStore";
+import { useAuth } from "./store/authStore";
+import Layout from "./components/Layout";
+import AppRoutes from "./routes/AppRoutes";
 
+export default function App() {
+  const setUser = useAuth((s) => s.setUser);
 
-export default function App({ Component, pageProps }: AppProps) {
-const setUser = useAuthStore((s) => s.setUser);
+  useEffect(() => {
+    // try to fetch current user on app load
+    api
+      .get("/auth/me", { withCredentials: true })
+      .then((res) => setUser(res.data))
+      .catch(() => setUser(null));
+  }, [setUser]);
 
-
-useEffect(() => {
-// try fetch current user on app load
-api
-.get("/auth/me")
-.then((res) => setUser(res.data))
-.catch(() => setUser(null));
-}, [setUser]);
-
-
-return (
-<QueryClientProvider client={queryClient}>
-<Layout>
-<Component {...pageProps} />
-</Layout>
-</QueryClientProvider>
-);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
