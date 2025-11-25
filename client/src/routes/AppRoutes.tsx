a22a8b2ada12368f9baac50297e1231ca09e69fc
de-scientist <gitaumark502@gmail.com>
2025-11-25 10:14:31 +0300

    import TrashPage from "@/pages/dashboard/trash";

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -8,12 +8,12 @@ import RegisterPage from "@/pages/auth/Register";
 import Dashboard from "@/pages/dashboard";
 import CreateBlogPage from "@/pages/dashboard/create";
 import BlogList from "@/pages/blogs/List";
-import TrashPage from "@/pages/dashboard/trash";
+//import TrashPage from "@/pages/dashboard/trash";
 
 import ProfilePage from "@/pages/profile";               // update profile
 import ViewProfilePage from "@/pages/profile/View";      // view profile
 import UserBlogsPage from "@/pages/profile/UserBlogs";   // user's blogs
-import UserTrashPage from "@/pages/profile/Trash";       // user trash
+import TrashPage from "@/pages/dashboard/trash";     // user trash
 import DeleteProfilePage from "@/pages/profile/DeleteProfilePage";  // soft delete
 import PermanentDeleteUserPage from "@/pages/profile/PermantDeleteUserPage";// hard delete
 
@@ -30,13 +30,13 @@ export default function AppRoutes() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/create" element={<CreateBlogPage />} />
       <Route path="/dashboard/list" element={<BlogList />} />
-      <Route path="/dashboard/trash" element={<TrashPage />} />
+      
 
       {/* Profile & User Management */}
       <Route path="/profile" element={<ProfilePage />} />                 {/* update */}
       <Route path="/profile/view" element={<ViewProfilePage />} />        {/* read */}
       <Route path="/profile/blogs" element={<UserBlogsPage />} />         {/* user blogs */}
-      <Route path="/profile/trash" element={<UserTrashPage />} />         {/* user trash */}
+      <Route path="/profile/trash" element={<TrashPage />} />         {/* user trash */}
       <Route path="/profile/delete" element={<DeleteProfilePage />} />    {/* soft delete */}
       <Route
         path="/profile/delete/permanent/:id"
