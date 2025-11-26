77dbbdb9825f677c703e7f6ce72aeb9790c1e6b7
de-scientist <gitaumark502@gmail.com>
2025-11-26 10:43:33 +0300

    add an image

--- client/src/pages/Inspiration.tsx
+++ client/src/pages/Inspiration.tsx
@@ -1,4 +1,3 @@
-// src/pages/Inspiration.tsx
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
