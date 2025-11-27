96c8b1b44e5316d7baaac7ff73d7bf75d14f6800
de-scientist <gitaumark502@gmail.com>
2025-11-27 20:50:30 +0300

    fix: Cannot find name 'button'.ts(2304) (property) JSX.IntrinsicElements.button: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> by import React from "react";

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react"; // 💡 Import useEffect
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "@/store/authStore";
