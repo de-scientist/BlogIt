5b2bded70f48018189eb2c045da0a8577d09bc36
de-scientist <gitaumark502@gmail.com>
2025-11-27 14:46:33 +0300

    add this app.use(express.json({ limit: "10mb" })); app.use(express.urlencoded({ extended: true, limit: "10mb" }));

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,8 @@ dotenv.config();
 const app: Express = express();
 
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 // Enable CORS for frontend
